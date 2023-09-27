11692bbadea7262e1f4bd2bb446d2e989e42bb8e
OzaRu <aruberuto03@gmail.com>
2023-09-27 21:02:54 +0200

    commit

--- Practica3_Alberto_Rodriguez/app/src/main/java/com/example/practica3_alberto_rodriguez/MainActivity.java
+++ Practica3_Alberto_Rodriguez/app/src/main/java/com/example/practica3_alberto_rodriguez/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.practica3_alberto_rodriguez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txt;
+    private ImageView img;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txt = findViewById(R.id.delEj1);
+        img = findViewById(R.id.cartel);
+        img.setOnClickListener(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+        txt.append(" Te diste cuentaaa");
     }
 }
