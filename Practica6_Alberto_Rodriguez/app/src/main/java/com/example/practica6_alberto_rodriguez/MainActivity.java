6007ecb2d89424c514c27a9812138b78873891be
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-10-10 13:36:18 +0200

    commit

--- Practica6_Alberto_Rodriguez/app/src/main/java/com/example/practica6_alberto_rodriguez/MainActivity.java
+++ Practica6_Alberto_Rodriguez/app/src/main/java/com/example/practica6_alberto_rodriguez/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.practica6_alberto_rodriguez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText nombre, apellidos, nacimiento, direccion, telefono;
+    TextView error;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        error = findViewById(R.id.tError);
         nombre = findViewById(R.id.tNombre);
         apellidos = findViewById(R.id.tApellidos);
         nacimiento = findViewById(R.id.tNacimiento);
@@ -36,9 +43,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(nombre.getText().length() != 0 && apellidos.getText().length() != 0 && nacimiento.getText().length() != 0
-           && direccion.getText().length() != 0 && telefono.getText().length() != 0){
+        boolean valido = true;
+        error.setText("");
 
+        if(String.valueOf(nombre.getText()).length() == 0){
+            error.append("Nombre -");
+            valido = false;
+        }
+        if(String.valueOf(apellidos.getText()).length() == 0){
+            error.append("Apellidos -");
+            valido = false;
+        }
+        if(String.valueOf(nacimiento.getText()).length() == 0){
+            error.append("Fecha -");
+            valido = false;
+        }
+        if(String.valueOf(direccion.getText()).length() == 0){
+            error.append("Dirección -");
+            valido = false;
+        }
+        if(String.valueOf(telefono.getText()).length() != 9){
+            error.append("Telefono -");
+            valido = false;
+        }
+
+        if(!valido){
+            error.append(" || Campo/s vacío/s o no válidos, revise los datos.");
+        }
+        else {
+            error.setText("");
+
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+            formatoFecha.setLenient(false);
+
+            try {
+                formatoFecha.parse(String.valueOf(nacimiento.getText()));
+
+            } catch (Exception e) {
+                error.setText("La fecha introducida no es válida.");
+            }
+        }
+
+
+        if(error.getText().length() == 0){
+            Bundle bolsa = new Bundle();
+            bolsa.putString("nombre", String.valueOf(nombre.getText()));
+            bolsa.putString("apellidos", String.valueOf(apellidos.getText()));
+            bolsa.putString("nacimiento", String.valueOf(nacimiento.getText()));
+            bolsa.putString("direccion", String.valueOf(direccion.getText()));
+            bolsa.putString("telefono", String.valueOf(telefono.getText()));
+
+            Intent intent = new Intent(this, ResumenActivity.class);
+            intent.putExtra("bolsa", bolsa);
+
+            startActivity(intent);
+            onPause();
         }
     }
 }
--- Practica6_Alberto_Rodriguez/app/src/main/java/com/example/practica6_alberto_rodriguez/ResumenActivity.java
+++ Practica6_Alberto_Rodriguez/app/src/main/java/com/example/practica6_alberto_rodriguez/ResumenActivity.java
@@ -2,13 +2,40 @@ package com.example.practica6_alberto_rodriguez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResumenActivity extends AppCompatActivity {
 
+    TextView muestra;
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        muestra = findViewById(R.id.tMuestra);
+        Bundle dRecogidos = getIntent().getBundleExtra("bolsa");
+
+        if (dRecogidos != null) {
+            muestra.setText("\n\nNombre: "+ dRecogidos.getString("nombre")+
+                            "\n\nApellidos: "+ dRecogidos.getString("apellidos")+
+                            "\n\nNacimiento: "+ dRecogidos.getString("nacimiento")+
+                            "\n\nDirección: "+ dRecogidos.getString("direccion")+
+                            "\n\nTeléfono: "+ dRecogidos.getString("telefono")+"\n");
+        }
     }
 }
