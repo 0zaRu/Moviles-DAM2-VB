cc4a4a0bb8a790801170558284f1ad1972f0239e
OzaRu <aruberuto03@gmail.com>
2023-11-06 19:20:05 +0100

    commit

---
+++ Practica10/Practica10_EjercicioA_Alberto_Rodriguez/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practica10_EjercicioA_Alberto_Rodriguez"
+include(":app")
+ 
