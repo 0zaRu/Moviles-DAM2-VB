013cbd769e27122869bc1aab4047fde18d32ba52
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-12-12 18:57:01 +0100

    commit

--- Practica16_Alberto_Rodriguez/app/src/main/java/com/example/practica16_alberto_rodriguez/MainActivity.java
+++ Practica16_Alberto_Rodriguez/app/src/main/java/com/example/practica16_alberto_rodriguez/MainActivity.java
@@ -1,32 +1,63 @@
 package com.example.practica16_alberto_rodriguez;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.practica16_alberto_rodriguez.Dialogs.DialogModify;
 import com.example.practica16_alberto_rodriguez.Dialogs.OnDialogEvent;
+import com.example.practica16_alberto_rodriguez.Fragments.DetailFragment;
+import com.example.practica16_alberto_rodriguez.Fragments.ListFragment;
 import com.example.practica16_alberto_rodriguez.Fragments.OnFragmentEventListener;
 import com.example.practica16_alberto_rodriguez.Coche.Coche;
 
 public class MainActivity extends AppCompatActivity implements OnFragmentEventListener, OnDialogEvent {
 
+    SQLHelper db = new SQLHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.mainButton).setOnClickListener(v -> {
+        estableceFrameList();
+
+        findViewById(R.id.floatAddButton).setOnClickListener(v -> {
             DialogModify dialog = new DialogModify();
             dialog.show(getSupportFragmentManager(), "ejemplo");
         });
 
     }
 
+    public void estableceFrameList(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        ListFragment lf = new ListFragment(db.selectCoches(null));
+        transaction.replace(R.id.frameList, lf);
+
+        transaction.commit();
+
+
+    }
+
     @Override
     public void estableceCoche(Coche c) {
-        //Este se encarga de poner el coche en el fragment details
+        /*if(getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            DetailFragment df = new DetailFragment(db.selectCoches(c.getNumBastidor()).get(0));
+            transaction.replace(R.id.frameDetalles, df);
+
+            transaction.commit();
+        }
+
+         */
     }
 
     @Override
--- Practica16_Alberto_Rodriguez/app/src/main/java/com/example/practica16_alberto_rodriguez/SQLHelper.java
+++ Practica16_Alberto_Rodriguez/app/src/main/java/com/example/practica16_alberto_rodriguez/SQLHelper.java
@@ -2,6 +2,7 @@ package com.example.practica16_alberto_rodriguez;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -56,6 +57,26 @@ public class SQLHelper extends SQLiteOpenHelper {
         listaDeCoches.add(new Coche("34567890123456789", "Honda", "Accord", "gasolina", "blanco", 24000));
         listaDeCoches.add(new Coche("45678901234567890", "Chevrolet", "Malibu", "híbrido", "negro", 20000));
         listaDeCoches.add(new Coche("56789012345678901", "Volkswagen", "Passat", "diésel", "azul", 26000));
+        listaDeCoches.add(new Coche("11111111111111111", "Audi", "A3", "gasolina", "negro", 25000));
+        listaDeCoches.add(new Coche("22222222222222222", "BMW", "X5", "diésel", "blanco", 40000));
+        listaDeCoches.add(new Coche("33333333333333333", "Mercedes", "C-Class", "híbrido", "rojo", 18000));
+        listaDeCoches.add(new Coche("44444444444444444", "Volkswagen", "Tiguan", "gasolina", "azul", 30000));
+        listaDeCoches.add(new Coche("55555555555555555", "Ford", "Explorer", "diésel", "gris", 35000));
+        listaDeCoches.add(new Coche("66666666666666666", "Hyundai", "Tucson", "gasolina", "verde", 22000));
+        listaDeCoches.add(new Coche("77777777777777777", "Toyota", "Rav4", "híbrido", "amarillo", 28000));
+        listaDeCoches.add(new Coche("88888888888888888", "Kia", "Sportage", "gasolina", "negro", 32000));
+        listaDeCoches.add(new Coche("99999999999999999", "Nissan", "Rogue", "diésel", "rojo", 27000));
+        listaDeCoches.add(new Coche("10101010101010101", "Chevrolet", "Equinox", "híbrido", "azul", 23000));
+        listaDeCoches.add(new Coche("12121212121212121", "Mazda", "CX-5", "gasolina", "blanco", 26000));
+        listaDeCoches.add(new Coche("13131313131313131", "Subaru", "Outback", "diésel", "gris", 29000));
+        listaDeCoches.add(new Coche("14141414141414141", "Honda", "HR-V", "gasolina", "verde", 21000));
+        listaDeCoches.add(new Coche("15151515151515151", "Ford", "Escape", "híbrido", "amarillo", 24000));
+        listaDeCoches.add(new Coche("16161616161616161", "Jeep", "Cherokee", "diésel", "rojo", 33000));
+        listaDeCoches.add(new Coche("17171717171717171", "Volkswagen", "Atlas", "gasolina", "azul", 36000));
+        listaDeCoches.add(new Coche("18181818181818181", "Audi", "Q5", "híbrido", "negro", 19000));
+        listaDeCoches.add(new Coche("19191919191919191", "Toyota", "Highlander", "diésel", "blanco", 31000));
+        listaDeCoches.add(new Coche("20202020202020202", "Mitsubishi", "Outlander", "gasolina", "gris", 27000));
+        listaDeCoches.add(new Coche("21212121212121212", "Buick", "Enclave", "híbrido", "verde", 24000));
 
         for(Coche c: listaDeCoches){
             insertaCoche(db, c);
@@ -76,5 +97,31 @@ public class SQLHelper extends SQLiteOpenHelper {
         }
 
         db.insert(CocheContract.TABLE_NAME, null, values);
+    }
+
+    public ArrayList<Coche> selectCoches(@Nullable String numBastidor){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = null;
+        if(numBastidor == null || numBastidor.isEmpty())
+            c = db.query(CocheContract.TABLE_NAME, null, null, null, null, null, null);
+
+        else
+            c = db.query(CocheContract.TABLE_NAME, null, CocheContract.NUM_BASTIDOR +" LIKE ?", new String[]{numBastidor}, null, null, null);
+
+
+        ArrayList<Coche> coches = new ArrayList<>();
+
+        while(c.moveToNext()){
+            coches.add(new Coche(
+                    c.getString(c.getColumnIndexOrThrow(CocheContract.NUM_BASTIDOR)),
+                    c.getString(c.getColumnIndexOrThrow(CocheContract.MARCA)),
+                    c.getString(c.getColumnIndexOrThrow(CocheContract.MODELO)),
+                    c.getString(c.getColumnIndexOrThrow(CocheContract.COMBUSTIBLE)),
+                    c.getString(c.getColumnIndexOrThrow(CocheContract.COLOR)),
+                    c.getInt(c.getColumnIndexOrThrow(CocheContract.KILOMETRAJE))
+            ));
+        }
+
+        return coches;
     }
 }
